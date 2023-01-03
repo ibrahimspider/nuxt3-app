628e57dc9a4abcd88a62f9b56538d168ec7a1447
unknown <ibrahimmosstafaspider@gmail.com>
2023-01-03 11:11:34 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Hoem</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptas
+      laudantium dicta accusamus harum aspernatur reprehenderit rem.
+      Perferendis, a voluptatibus!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptas
+      laudantium dicta accusamus harum aspernatur reprehenderit rem.
+      Perferendis, a voluptatibus!
+    </p>
+  </div>
+</template>
+<script setup>
+</script>
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products" :key="p">
+        <ProductCard :product="p"/>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+  definePageMeta({
+    layout: 'products'
+  })
+
+  const { data : products } = await useFetch('https://fakestoreapi.com/products')
+
+  useHead({
+    title: 'Nuxt Dojo | Merch',
+    meta: [
+                {name: 'description', content: 'Nuxt 3 Merch'}
+            ]
+  })
+
+</script>
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36;
+}
+p {
+  margin: 20px 0;
+}
+</style>
